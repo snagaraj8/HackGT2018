271e4d4041d39973c720330977adc9364d488dd1
Sanath Nagaraj <snagaraj8@gatech.edu>
2018-10-20 15:32:06 -0400

    created log in and registration page

--- app/src/main/java/com/image/hackgt/hackgt2018/MainActivity.java
+++ app/src/main/java/com/image/hackgt/hackgt2018/MainActivity.java
@@ -1,7 +1,10 @@
 package com.image.hackgt.hackgt2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cancelButton = findViewById(R.id.button_cancel);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCancelPressed();
+            }
+        });
+    }
+
+    private void onCancelPressed() {
+        Intent intent = new Intent(MainActivity.this,
+                UpdateActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/image/hackgt/hackgt2018/User.java
+++ app/src/main/java/com/image/hackgt/hackgt2018/User.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    String username;
-    String password;
-    String name;
-    String dob;
-    List<Double> preferences;
+    private String username;
+    private String password;
+    private String name;
+    private String dob;
+    private List<Double> preferences;
 
     public User(String username, String password, String name, String dob,
                 List<Double> preferences) {
@@ -17,5 +17,49 @@ public class User {
         this.name = name;
         this.dob = dob;
         this.preferences = new ArrayList<>(preferences);
+    }
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public List<Double> getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(List<Double> preferences) {
+        this.preferences = preferences;
     }
 }
