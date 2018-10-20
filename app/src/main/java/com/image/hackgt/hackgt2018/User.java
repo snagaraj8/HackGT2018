89349aa58666a02bcd410bb1b7aee32be7f41d6b
Aniruddha Das <aniruddhadas@lawn-128-61-48-250.lawn.gatech.edu>
2018-10-20 13:39:40 -0400

    Added constructor and attributes to User

--- app/src/main/java/com/image/hackgt/hackgt2018/User.java
+++ app/src/main/java/com/image/hackgt/hackgt2018/User.java
@@ -1,4 +1,21 @@
 package com.image.hackgt.hackgt2018;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    String username;
+    String password;
+    String name;
+    String dob;
+    List<Double> preferences;
+
+    public User(String username, String password, String name, String dob,
+                List<Double> preferences) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.dob = dob;
+        this.preferences = new ArrayList<>(preferences);
+    }
 }
